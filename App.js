580fddbd6159490ea5fbf512a6b0625f5cdcd9bc
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-28 21:47:45 +0100

    splash

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
+import SplashScreen from 'react-native-splash-screen';
 import App from './src/App';
 
 export default function Main() {
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <PaperProvider>
       <App />
