b237f8fc12ed25fda1dab54c4d8d672102c24fc1
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-28 21:46:26 +0100

    splash screen

--- android/app/src/main/java/com/icook/MainActivity.java
+++ android/app/src/main/java/com/icook/MainActivity.java
@@ -1,6 +1,7 @@
 package com.icook;
-
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +12,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "iCook";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
