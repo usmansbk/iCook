6595b620becefcaaaf59ab94f029f172e06272bd
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-28 21:26:59 +0100

    paper

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View} from 'react-native';
+import {Text} from 'react-native-paper';
+
+export default () => {
+  return (
+    <View>
+      <Text>Start</Text>
+    </View>
+  );
+};
