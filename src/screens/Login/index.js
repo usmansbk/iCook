0902e0e334c7ccb6e9a9c099ce378abc9aae7008
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-29 12:07:06 +0100

    chore

--- src/screens/Login/index.js
+++ src/screens/Login/index.js
@@ -7,49 +7,53 @@ import Logo from '../../common/Logo';
 import Button from '../../common/Button';
 import GoogleButton from '../../common/GoogleButton';
 import FacebookButton from '../../common/FacebookButton';
+import Header from '../../common/Header';
 import Or from '../../common/Or';
 import colors from '../../config/colors';
 
 export default () => {
   return (
-    <Container>
-      <View style={styles.container}>
-        <Logo />
-        <View>
-          <View style={styles.input}>
-            <TextInput
-              label="Email Address"
-              placeholder="Enter your email address"
-            />
-          </View>
-          <View style={styles.input}>
-            <TextInput
-              label="Password"
-              placeholder="Enter your password"
-              icon="eye"
-              secure
-            />
-            <TouchableOpacity style={styles.nav}>
-              <Text style={styles.actionText}>Forgot password?</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.button}>
-            <Button style={button} text="Log in" />
-          </View>
-          <Or />
-          <View style={styles.footer}>
-            <GoogleButton />
-            <FacebookButton />
-            <TouchableOpacity style={styles.nav}>
-              <Text style={styles.footerText}>
-                {"Don't have an account?"}{' '}
-                <Text style={styles.actionText}>Sign Up</Text>
-              </Text>
-            </TouchableOpacity>
+    <>
+      <Header title="Login" />
+      <Container>
+        <View style={styles.container}>
+          <Logo />
+          <View>
+            <View style={styles.input}>
+              <TextInput
+                label="Email Address"
+                placeholder="Enter your email address"
+              />
+            </View>
+            <View style={styles.input}>
+              <TextInput
+                label="Password"
+                placeholder="Enter your password"
+                icon="eye"
+                secure
+              />
+              <TouchableOpacity style={styles.nav}>
+                <Text style={styles.actionText}>Forgot password?</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.button}>
+              <Button style={button} text="Log in" />
+            </View>
+            <Or />
+            <View style={styles.footer}>
+              <GoogleButton />
+              <FacebookButton />
+              <TouchableOpacity style={styles.nav}>
+                <Text style={styles.footerText}>
+                  {"Don't have an account?"}{' '}
+                  <Text style={styles.actionText}>Sign Up</Text>
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
         </View>
-      </View>
-    </Container>
+      </Container>
+    </>
   );
 };
 
