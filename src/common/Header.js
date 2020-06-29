165ca94bc40b0113040173a0a292cc042f79550b
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-29 05:52:10 +0100

    chore

--- src/common/Header.js
+++ src/common/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet} from 'react-native';
 import {Appbar} from 'react-native-paper';
 import Text from './Text';
 import Icon from './Icon';
