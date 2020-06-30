c3920ae9d5326d822e2b98463261403e9361fb6a
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-30 18:28:14 +0100

    chore

--- src/screens/DishDetails/index.js
+++ src/screens/DishDetails/index.js
@@ -6,9 +6,16 @@ import mock from './mock';
 
 export default ({navigation}) => {
   const _goBack = React.useCallback(() => navigation.goBack(), [navigation]);
+  const _rightAction = () => null;
+  const {me = true} = {};
   return (
     <View style={styles.container}>
-      <Header goBack={_goBack} title="Recipe" />
+      <Header
+        goBack={_goBack}
+        title="Recipe"
+        rightIcon={me ? 'ellipsis1' : undefined}
+        rightAction={_rightAction}
+      />
       <Recipe data={mock} navigation={navigation} />
     </View>
   );
