bd9be3547c9c9ece18fe5d90cb0c5b0eaefa143d
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-06-30 05:27:54 +0100

    structure

--- src/lists/dishes/Item.js
+++ src/lists/dishes/Item.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import numeral from 'numeral';
-import {View, Image, StyleSheet, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from 'react-native';
 import Avatar from '../../common/Avatar';
 import Text from '../../common/Text';
 import IconButton from '../../common/IconButton';
@@ -24,42 +31,41 @@ export default class Item extends React.Component {
       big,
     } = this.props;
     return (
-      <TouchableOpacity
-        disabled={disabled}
-        onPress={this._onPress}
-        style={styles.container}>
-        <View style={styles.header}>
-          <View style={styles.avatar}>
-            <Avatar uri={authorAvatar} name={authorAvatar} size="h3" />
-          </View>
-          <Text numberOfLines={1} style={styles.name}>
-            {authorName}
-          </Text>
-          <Text style={styles.date}>
-            {formatDistance(new Date(date), {addSuffix: true})}
-          </Text>
-        </View>
-        <Image source={{uri: image}} style={styles.image} />
-        <View style={styles.footer}>
-          <View style={styles.buttons}>
-            <View style={styles.row}>
-              <IconButton
-                color={isLiked ? colors.veryWeakRed : undefined}
-                name={`heart${isLiked ? '' : 'o'}`}
-              />
-              <IconButton name="message1" />
+      <TouchableNativeFeedback disabled={disabled} onPress={this._onPress}>
+        <View style={styles.container}>
+          <View style={styles.header}>
+            <View style={styles.avatar}>
+              <Avatar uri={authorAvatar} name={authorAvatar} size="h3" />
             </View>
-            <IconButton name="sharealt" color={colors.primary} />
-          </View>
-          <View style={styles.footerBody}>
-            <Text style={styles.boldText}>
-              {numeral(likes).format('0.0a')} likes
+            <Text numberOfLines={1} style={styles.name}>
+              {authorName}
             </Text>
-            <Text style={styles.title}>{title}</Text>
-            <Text numberOfLines={big ? undefined : 2}>{description}</Text>
+            <Text style={styles.date}>
+              {formatDistance(new Date(date), {addSuffix: true})}
+            </Text>
+          </View>
+          <Image source={{uri: image}} style={styles.image} />
+          <View style={styles.footer}>
+            <View style={styles.buttons}>
+              <View style={styles.row}>
+                <IconButton
+                  color={isLiked ? colors.veryWeakRed : undefined}
+                  name={`heart${isLiked ? '' : 'o'}`}
+                />
+                <IconButton name="message1" />
+              </View>
+              <IconButton name="sharealt" color={colors.primary} />
+            </View>
+            <View style={styles.footerBody}>
+              <Text style={styles.boldText}>
+                {numeral(likes).format('0.0a')} likes
+              </Text>
+              <Text style={styles.title}>{title}</Text>
+              <Text numberOfLines={big ? undefined : 2}>{description}</Text>
+            </View>
           </View>
         </View>
-      </TouchableOpacity>
+      </TouchableNativeFeedback>
     );
   }
 }
