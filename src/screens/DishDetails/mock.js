516c9ae1c8fb9025fb8c8f5d862f880e1e2c64b9
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-02 05:25:49 +0100

    chore

--- src/screens/DishDetails/mock.js
+++ src/screens/DishDetails/mock.js
@@ -20,55 +20,55 @@ export default {
   createdAt: new Date().toISOString(),
   ingredients: [
     {
-      id: 1,
+      id: '1',
       quantity: 3,
       unit: 'cups',
       of: 'flour',
     },
     {
-      id: 2,
+      id: '2',
       quantity: 1,
       unit: 'teaspoon',
       of: 'salt',
     },
     {
-      id: 3,
+      id: '3',
       quantity: 5,
       unit: 'teaspoon',
       of: 'baking powder',
     },
     {
-      id: 4,
+      id: '4',
       quantity: 1,
       unit: 'teaspoon',
       of: 'cinnamon - powdered',
     },
     {
-      id: 5,
+      id: '5',
       quantity: 1,
       unit: 'teaspoon',
       of: 'nutmeg - powdered',
     },
     {
-      id: 6,
+      id: '6',
       quantity: 2,
       unit: 'tablespoon',
       of: 'butter',
     },
     {
-      id: 7,
+      id: '7',
       quantity: 3,
       unit: 'cups',
       of: 'flour',
     },
     {
-      id: 8,
+      id: '8',
       quantity: 2,
       unit: '',
       of: 'eggs beaten well',
     },
     {
-      id: 9,
+      id: '9',
       quantity: 3,
       unit: 'cups',
       of: 'milk',
@@ -76,7 +76,7 @@ export default {
   ],
   steps: [
     {
-      id: 1,
+      id: '1',
       step: 1,
       do:
         'Sift flour with salt, baking powder, cinnamon and nutmeg and keep aside',
@@ -84,7 +84,7 @@ export default {
         'https://media.istockphoto.com/photos/homemade-glazed-yeast-donuts-picture-id1198710908?b=1&k=6&m=1198710908&s=170667a&w=0&h=q4SwuRnVrkb72lNYlmvtA0HyCzPQHAOm1qLSVmUvpwg=',
     },
     {
-      id: 2,
+      id: '2',
       step: 2,
       do: 'Cream butter and sugar, Add eggs and beat well',
       image:
@@ -126,25 +126,25 @@ export default {
   ],
   healthBenefits: [
     {
-      id: 1,
+      id: '1',
       gain: 'They are highly beneficial for your mental well being',
     },
     {
-      id: 2,
+      id: '2',
       gain:
         'Same researchers say that the donut and coffee combo could help enhance your memory',
     },
     {
-      id: 3,
+      id: '3',
       gain:
         'Not only are they fried, but full of sugar. Sugar gives you energy',
     },
     {
-      id: 4,
+      id: '4',
       gain: 'Source of Calcium',
     },
     {
-      id: 5,
+      id: '5',
       gain: 'Source of Iron',
     },
   ],
