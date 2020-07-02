beddcbf31baf88448ee68f2032b89004824b6268
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-02 03:05:26 +0100

    form

--- src/forms/food/index.js
+++ src/forms/food/index.js
@@ -1,11 +1,50 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
-import Text from '../../common/Text';
+import TextInput from '../../common/TextInput';
 
 export default () => {
   return (
     <View style={styles.container}>
-      <Text>Title</Text>
+      <View style={styles.field}>
+        <TextInput required label="Title" placeholder="Food name" />
+      </View>
+      <View style={styles.field}>
+        <TextInput
+          label="Category"
+          required
+          placeholder="Lunch, Vegan, Gluten-Free, or anything"
+        />
+      </View>
+      <View style={styles.field}>
+        <TextInput
+          label="Price"
+          required
+          keyboardType="number-pad"
+          placeholder="Reasonable price"
+        />
+      </View>
+      <View style={styles.field}>
+        <TextInput
+          label="Description"
+          required
+          multiline
+          placeholder="That will capture your customers hearts"
+        />
+      </View>
+      <View style={styles.field}>
+        <TextInput
+          label="Ingredients"
+          placeholder="Add ingredient"
+          icon="pluscircleo"
+        />
+      </View>
+      <View style={styles.field}>
+        <TextInput
+          label="Health Benefits"
+          placeholder="Add benefit"
+          icon="pluscircleo"
+        />
+      </View>
     </View>
   );
 };
@@ -15,5 +54,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'white',
     padding: 20,
+  },
+  field: {
+    marginVertical: 10,
   },
 });
